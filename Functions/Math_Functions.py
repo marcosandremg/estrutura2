96ae0f84157c3303481a54e6da93a51b97323b18
marco <marcos131962@yahoo.com.br>
2022-03-11 16:54:29 -0300

    New functions

---
+++ Functions/Math_Functions.py
@@ -0,0 +1,12 @@
+class Operators:
+    def __init__(self):
+        self.name = "Letra a"
+
+    def __len__(self):
+        return 2
+
+    def sum(self, x, y):
+        return x + y
+
+    def diference(self, x, y):
+        return x - y
